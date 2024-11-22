c40257bbe39e0dd10a9268d9627d72091a32e5cf
Kamruzzaman Tutul <kamruzzaman103>
2024-11-22 15:48:55 +0600

    update 1

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -8,16 +8,18 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading]=useState(true);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
     return () => unsubscribe();
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, loginWithGoogle, logout }}>
+    <AuthContext.Provider value={{ user,loading, loginWithGoogle, logout}}>
       {children}
     </AuthContext.Provider>
   );
